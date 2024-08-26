2ef92662b87094bf7a8c9e18d0c01f3423c4b559
Michal Torma <miso.torma.dev@gmail.com>
2024-08-26 12:45:40 +0200

    Filter out only with media and make subset

--- 10_create_subset.py
+++ 10_create_subset.py
@@ -16,16 +16,21 @@ with DwCAReader(file_path) as dwca:
 #%%
 df['datasetKey'].unique()
 # %%
+df = df[df['datasetKey'] != '6134']
+#%%
+df_with_media = df[df['mediaType'].notna()]
 
 
 #%% Group by 'datasetKey' and sample 200 rows for each group
-subset_df = filtered_df.groupby('datasetKey').apply(lambda x: x.sample(n=200, replace=True)).reset_index(drop=True)
+subset_df = df_with_media.groupby('datasetKey').apply(lambda x: x.sample(n=200, replace=True)).reset_index(drop=True)
 # %%
 subset_df.to_csv('data_subset.csv')
 # %%
 subset_df = pd.read_csv('data_subset.csv')
 # %%
-multimedia = pd.read_csv('/itf-fi-ml/home/michato/data/multimedia.txt', sep='\t')
 # %%
-multimedia[multimedia['gbifID'].isin(subset_df['gbifID'])]
+multimedia_df[multimedia_df['coreid'].isin(subset_df['id'])]
 # %%
+subset_with_media = pd.merge(left=subset_df, right=multimedia_df, left_on='id', right_on='coreid', how='left')
+#%%
+subset_with_media.to_csv('data/data_subset.csv')
